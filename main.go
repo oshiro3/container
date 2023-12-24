85b67683207b3481cbd53b806ce4adf7b8c4e13d
YHojo <hojo.yosuke@gmail.com>
2023-12-24 19:38:48 +0900

    init version

---
+++ main.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/opencontainers/runc/libcontainer"
+	"github.com/opencontainers/runc/libcontainer/configs"
+	"github.com/opencontainers/runc/libcontainer/devices"
+	_ "github.com/opencontainers/runc/libcontainer/nsenter"
+	"github.com/opencontainers/runc/libcontainer/specconv"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
+)
+
+func init() {
+	if len(os.Args) > 1 && os.Args[1] == "init" {
+		runtime.GOMAXPROCS(1)
+		runtime.LockOSThread()
+		factory, _ := libcontainer.New("")
+		logrus.Info("init")
+		if err := factory.StartInitialization(); err != nil {
+			logrus.Fatal(err)
+		}
+		panic("--this line should have never been executed, congratulations--")
+	}
+}
+
+func main() {
+	abs, _ := filepath.Abs("./")
+
+	defaultMountFlags := unix.MS_NOEXEC | unix.MS_NOSUID | unix.MS_NODEV
+	var devices []*devices.Rule
+	for _, device := range specconv.AllowedDevices {
+		devices = append(devices, &device.Rule)
+	}
+
+	capabilities := []string{"CAP_CHOWN",
+		"CAP_DAC_OVERRIDE",
+		"CAP_FSETID",
+		"CAP_FOWNER",
+		"CAP_MKNOD",
+		"CAP_NET_RAW",
+		"CAP_SETGID",
+		"CAP_SETUID",
+		"CAP_SETFCAP",
+		"CAP_SETPCAP",
+		"CAP_NET_BIND_SERVICE",
+		"CAP_SYS_CHROOT",
+		"CAP_KILL",
+		"CAP_AUDIT_WRITE",
+	}
+
+	config := &configs.Config{
+		Rootfs: abs + "/dev/rootfs",
+		Capabilities: &configs.Capabilities{
+			Bounding:    capabilities,
+			Effective:   capabilities,
+			Inheritable: capabilities,
+			Permitted:   capabilities,
+			Ambient:     capabilities,
+		},
+		Namespaces: configs.Namespaces([]configs.Namespace{
+			{Type: configs.NEWNS},
+			{Type: configs.NEWUTS},
+			{Type: configs.NEWIPC},
+			{Type: configs.NEWPID},
+			{Type: configs.NEWUSER},
+			{Type: configs.NEWNET},
+			{Type: configs.NEWCGROUP},
+		}),
+		Cgroups: &configs.Cgroup{
+			Name:   "test-container",
+			Parent: "system",
+			Resources: &configs.Resources{
+				MemorySwappiness: nil,
+				Devices:          devices,
+			},
+		},
+		MaskPaths: []string{
+			"/proc/kcore",
+			"/sys/firmware",
+		},
+		ReadonlyPaths: []string{
+			"/proc/sys", "/proc/sysrq-trigger", "/proc/irq", "/proc/bus",
+		},
+		Devices:  specconv.AllowedDevices,
+		Hostname: "testing",
+		Mounts: []*configs.Mount{
+			{
+				Source:      "proc",
+				Destination: "/proc",
+				Device:      "proc",
+				Flags:       defaultMountFlags,
+			},
+			{
+				Source:      "tmpfs",
+				Destination: "/dev",
+				Device:      "tmpfs",
+				Flags:       unix.MS_NOSUID | unix.MS_STRICTATIME,
+				Data:        "mode=755",
+			},
+			{
+				Source:      "devpts",
+				Destination: "/dev/pts",
+				Device:      "devpts",
+				Flags:       unix.MS_NOSUID | unix.MS_NOEXEC,
+				Data:        "newinstance,ptmxmode=0666,mode=0620,gid=5",
+			},
+			{
+				Device:      "tmpfs",
+				Source:      "shm",
+				Destination: "/dev/shm",
+				Data:        "mode=1777,size=65536k",
+				Flags:       defaultMountFlags,
+			},
+			{
+				Source:      "mqueue",
+				Destination: "/dev/mqueue",
+				Device:      "mqueue",
+				Flags:       defaultMountFlags,
+			},
+			{
+				Source:      "sysfs",
+				Destination: "/sys",
+				Device:      "sysfs",
+				Flags:       defaultMountFlags | unix.MS_RDONLY,
+			},
+		},
+		UidMappings: []configs.IDMap{
+			{
+				ContainerID: 0,
+				HostID:      1000,
+				Size:        65536,
+			},
+		},
+		GidMappings: []configs.IDMap{
+			{
+				ContainerID: 0,
+				HostID:      1000,
+				Size:        65536,
+			},
+		},
+		Networks: []*configs.Network{
+			{
+				Type:    "loopback",
+				Address: "127.0.0.1/0",
+				Gateway: "localhost",
+			},
+		},
+		Rlimits: []configs.Rlimit{
+			{
+				Type: unix.RLIMIT_NOFILE,
+				Hard: uint64(1025),
+				Soft: uint64(1025),
+			},
+		},
+	}
+
+	factory, err := libcontainer.New(abs+"/dev/rootfs", libcontainer.InitArgs(os.Args[0], "init"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	container, err := factory.Create("test-container", config)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	process := &libcontainer.Process{
+		Args:   []string{"/bin/ash"},
+		Env:    []string{"PATH=/bin"},
+		User:   "daemon",
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+		Init:   true,
+	}
+
+	err = container.Run(process)
+	if err != nil {
+		container.Destroy()
+		logrus.Fatal(err)
+		return
+	}
+
+	_, err = process.Wait()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	container.Destroy()
+}
